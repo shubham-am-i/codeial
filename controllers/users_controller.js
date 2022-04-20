21601e7a1b1f0f3c68beff313c4e0ecad19a4798
Shubham <patilshubham2k21@gmail.com>
2022-04-20 07:57:31 +0530

    write sign-up code in controller and saved to database

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+const User = require('../models/user');
+
+
 module.exports.profile = function(req, res){
     res.end('<h1> User Profile</h1>')
 }
@@ -10,4 +13,32 @@ module.exports.signUp = function(req, res){
 // render sign in
 module.exports.signIn = function(req, res){
     return res.render('user_sign_in', { title: "Codeial | sign in"});
+}
+
+
+// get the sign up data
+module.exports.create = function(req, res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err) {console.log('error in finding user in signing up'); return;}
+
+        if(!user){
+            console.log(req.body);
+            User.create(req.body, function(err, user){
+                if(err) { console.log('error in creating user while signing up'); return;}
+
+                return res.redirect('/users/sign-in');
+            });
+        } else {
+            return res.redirect('back');
+        }
+    });
+}
+
+// sign in and create a session for the user
+module.exports.createSession = function(req, res){
+    // Todo later
 }
--- index.js
+++ index.js
@@ -1,20 +1,27 @@
+// requiring dependecies of packages
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const { engine } = require('express/lib/application');
+const expressLayouts = require('express-ejs-layouts');
 const app = express()
 const port = 8000;
 // require database
 const db = require('./config/mongoose');
 
 
-// set up static folder, express-ejs-Layouts and use it
-app.use(express.static('assests'));
-const expressLayouts = require('express-ejs-layouts');
+// set up static folder, express-ejs-Layouts,cookie-parser, body-parser and use it
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.static('./assets'));
 app.use(expressLayouts);
+
+
 // extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-//  use express router
+
+//  use express router [sequence ==> index.js => routes => controllers => views]
 app.use('/', require('./routes'));
 
 // set view engine
